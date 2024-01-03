4e8c42464a13c670b56a8e797b5852ca6e03578c
m0h4mm3d-0 <58182483+m4hunt3r@users.noreply.github.com>
2024-01-03 12:31:27 +0100

    cheated to solve this one from thePrimeagen stream

--- 2023/04_Scratchcards/solultion.js
+++ 2023/04_Scratchcards/solultion.js
@@ -1,5 +1,37 @@
 const fs = require('fs');
 const fileName = "in.txt";
+let result = 0;
 
-const contents = fs.readFileSync(fileName, 'utf8').split(/\n/g).map(x => x.split('')).filter(x => x.length > 0);
+function tDict(numbers) {
+	return numbers.reduce((acc, x) => {
+		acc[x] = true;
+		return acc;
+	}, {})
+}
 
+const contents = fs.readFileSync(fileName, 'utf8').split("\n").filter(x => x.length).map(x => { 
+	const values = x.split(": ")[1];
+	console.log(values)
+	const [winners, numbers] = values.split(" | ");
+	const wDict = tDict(winners.split(" ").filter(x => x).map(x => parseInt(x)));
+
+	console.log(wDict)
+	return [wDict, numbers.split(" ").map(x => parseInt(x.trim()))];
+}).reduce((acc, [wDict, numbers]) => {
+	
+	let point = 0;
+	numbers.forEach(x => {
+		if (wDict[x]) {
+			if (point === 0) {
+				point = 1;
+			} else {
+				point <<= 1;
+			}
+		}
+	});
+
+	return acc + point ;
+}, 0);
+
+
+console.log(contents)
