278e0039aa0794dfb4ddc1c970df9a99a8455f0a
m0h4mm3d-0 <58182483+m4hunt3r@users.noreply.github.com>
2024-01-03 13:40:36 +0100

    solved part2 with the help of primeagen (more like cheating/copyPasting) but ok

--- 2023/04_Scratchcards/solultion.js
+++ 2023/04_Scratchcards/solultion.js
@@ -8,17 +8,13 @@ function tDict(numbers) {
 		return acc;
 	}, {})
 }
-
-const contents = fs.readFileSync(fileName, 'utf8').split("\n").filter(x => x.length).map(x => { 
+//part 1 
+const contents1 = fs.readFileSync(fileName, 'utf8').split("\n").filter(x => x.length).map(x => { 
 	const values = x.split(": ")[1];
-	console.log(values)
 	const [winners, numbers] = values.split(" | ");
 	const wDict = tDict(winners.split(" ").filter(x => x).map(x => parseInt(x)));
-
-	console.log(wDict)
 	return [wDict, numbers.split(" ").map(x => parseInt(x.trim()))];
 }).reduce((acc, [wDict, numbers]) => {
-	
 	let point = 0;
 	numbers.forEach(x => {
 		if (wDict[x]) {
@@ -29,9 +25,45 @@ const contents = fs.readFileSync(fileName, 'utf8').split("\n").filter(x => x.len
 			}
 		}
 	});
-
 	return acc + point ;
 }, 0);
+console.log("part1", contents1);
 
+//part 2
+const cards = fs.readFileSync(fileName, 'utf8').split("\n").filter(x => x.length);
 
-console.log(contents)
+function getPoints(x, index) {
+	const values = x.split(": ")[1];
+	const [winners, numbersValues] = values.split(" | ");
+	const wDict = tDict(winners.split(" ").filter(x => x).map(x => parseInt(x)));
+	const numbers = numbersValues.split(" ").map(x => parseInt(x.trim()))
+	let point = 0;
+	numbers.forEach(x => {
+		if (wDict[x]) {
+			point++;
+		}
+	});
+	return new Array(point).fill(index + 1).map((x, i) => {
+		return x + i
+	});
+}
+const process = new Array(cards.length).fill(0).map((_, i) => i + 1);
+const seen = {};
+const count = {};
+
+while (process.length) {
+	const idx = process.pop();
+	count[idx] = count[idx] ? count[idx] + 1 : 1;
+	const points = seen[idx] ? seen[idx] : getPoints(cards[idx - 1], idx);
+	seen[idx] = points;
+
+	points.forEach(x => {
+		process.push(x);
+	});
+}
+
+const resultat = Object.keys(count).reduce((acc, x) => {
+	return acc + count[x];
+}, 0);
+
+console.log("part2 ", resultat)
